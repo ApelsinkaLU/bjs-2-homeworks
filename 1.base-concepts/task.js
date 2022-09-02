fb9f45a80cfc39bb0a4e23249b19aa32c49a0cbe
Alina <hilfigershark@gmail.com>
2022-09-02 19:14:14 +0300

    add task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,33 @@
+/*jshint esversion: 7 */
+"use strict";
+
+
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let d = Math.pow(b, 2)-(4*a*c);
+
+  if (d < 0) {
+    return arr;
+  } else if (d == 0) {
+    arr[0] = (-b/2*a);
+  } else if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d) )/(2*a);
+    arr[1] = (-b - Math.sqrt(d) )/(2*a);
+  }
+
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+solveEquation();
 
-  // код для задачи №2 писать здесь
 
-  return totalAmount;
-}
+
+
+// function calculateTotalMortgage(percent, contribution, amount, date) {
+//   let totalAmount;
+
+//   // код для задачи №2 писать здесь
+
+//   return totalAmount;
+// }
