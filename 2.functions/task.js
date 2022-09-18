5dabcb22461ee2b4d083064f687b189a67ff971c
Alina <hilfigershark@gmail.com>
2022-09-18 13:58:02 +0300

    task solution

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,70 @@
 // Задание 1
+/*jshint esversion: 7 */
+'use strict'
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
 
-  return { min: min, max: max, avg: avg };
+    sum =+ arr[i];
+
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  avg = +(sum / arr.length).toFixed(2);
+
+  return { min:min, max:max, avg:avg };
 }
+
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = arr[0];
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum =+ arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = func(arrOfArr[0]);
 
-  // Ваш кода
-  // for ...
-  
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sum = func(arrOfArr[i]);
+      if (sum > max) {
+        max = sum;
+      }
+  }
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+      
+      if (arr[i] < min) {
+        min = arr[i];
+      }
+  }
+
+  return Math.abs(min - max);
 }
+
