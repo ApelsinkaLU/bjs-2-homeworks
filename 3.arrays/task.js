f323e098218b527b1c8d6c5f094a4fb9d1de97e5
Alina <hilfigershark@gmail.com>
2022-09-22 14:37:52 +0300

    arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,20 @@
+/*jshint esversion: 7 */
+
+// Задача 1
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if (arr1.length === arr2.length) {
+    result = arr1.every((num, i) => num === arr2[i]);
+  } else result = false;
 
   return result; // boolean
 }
 
-function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
+// Задача 2
+function advancedFilter(arr) {
+  let resultArr = arr.filter((number) => number > 0 && number % 3 === 0).map((number) => number * 10);
 
   return resultArr; // array
 }
