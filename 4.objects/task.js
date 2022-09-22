ce7c412cca84c34aa7d74c4909fe86fc0b93d6c2
Alina <hilfigershark@gmail.com>
2022-09-22 17:51:28 +0300

    objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,53 @@
-function Student(name, gender, age) {
-    // Ваш код
+/*jshint esversion: 7 */
 
+function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
+
+// let student1 = new Student('Маша', 'female', 19);
+// let student2 = new Student('Костя', 'male', 24);
+
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if(this.marks === undefined){ 
+      this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+};
+
+Student.prototype.addMarks = function (...marks) {
+  if(this.marks === undefined){ 
+    this.marks = marks;
+  } else {
+    this.marks.push(...marks);
+  }
+};
+
+Student.prototype.getAverage = function () {
+  if(this.marks.length === 0) {
+    return 0;
+  } else {
+    let sum = 0;
+    let count = 0;
+      for (let i = 0; i < this.marks.length; i++) {
+        count += 1;
+        sum += this.marks[i];
+      }
+    
+    return sum/count;
+  }
+};
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+
+  this.excluded = reason;
+};
